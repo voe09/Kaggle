a17e6650f24f905f3566f31d83e866ac8f4a5a03
Haoyang Chen <haoychen@outlook.com>
2016-01-11 00:20:01 -0500

    Test collabration

---
+++ Titanic/Tianic.R
@@ -0,0 +1,16 @@
+#Test test test test
+library(randomForest)
+
+train<- read.csv('train.csv')
+test <- read.csv('test.csv')
+#gendermodel<- read.csv('gendermodel.csv')
+#genderclassmodel<- read.csv('genderclassmodel.csv')
+train.y<- as.factor(train$Survived)
+
+train.x<- train[,c(3,5,7,8,10,12)]
+try.test<- test[,c(2,4,6,7,9,11)]
+try.test[153,5]<- mean(try.test[,5], na.rm = T)
+
+rf<- randomForest(train.x, train.y,ntree = 500)
+
+rfpredice<- predict(rf, try.test)
